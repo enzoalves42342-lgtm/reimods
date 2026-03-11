5f95a1a088eb679ca79c9a9b162a1eee84508320
enzoalves42342-lgtm <enzoalves42342@gmail.com>
2026-03-11 17:16:43 -0300

    Update mp_webhook.js

--- netlify/functions/mp_webhook.js
+++ netlify/functions/mp_webhook.js
@@ -46,9 +46,9 @@ exports.handler = async (event) => {
       .join("\n") || "Não identificado";
 
     // DADOS DO COMPRADOR
-    const cliente = data.metadata?.cliente_nome || "Não informado";
-    const email = data.metadata?.cliente_email || "Não informado";
-    const celular = data.metadata?.cliente_celular || "Não informado";
+    const cliente = `${data.payer?.first_name || ""} ${data.payer?.last_name || ""}`.trim() || "Não informado";
+    const email = data.payer?.email || "Não informado";
+    const celular = data.payer?.phone?.number || "Não informado";
 
     const response = await fetch(process.env.DISCORD_WEBHOOK_URL, {
       method: "POST",
